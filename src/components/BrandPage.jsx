4f3d8ff757b7ac2f2d1a478586f6c503d5690542
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-20 11:47:08 +0600

    add advertigement in brandpage

--- src/components/BrandPage.jsx
+++ src/components/BrandPage.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import BrandCard from "./BrandCard";
 import { useEffect, useState } from "react";
+import Slider from "./Slider";
  
 
 const BrandPage = () => {
@@ -19,7 +20,7 @@ const BrandPage = () => {
         <p className="flex items-center justify-center text-3xl font-semibold min-h-screen">Products not available</p>
         ) : (
           <div>
-           
+           <Slider></Slider>
            
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             {cars?.map((car) => (
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,1 +1,35 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+const Slider = () => {
+  return (
+    <div className="my-16">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={2}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide className="relative">
+                  <img src="https://i.ibb.co/xHjgDgq/mercedez-640x426.webp" alt="" />
+                  <div className="absolute top-1/4 left-[70%]">
+                      <img className="w-1/2 lg:w-1/3 rounded-full" src="https://i.ibb.co/DD6xMJ8/discount.webp" alt="" />
+                  </div>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+                  <img src="https://i.ibb.co/YBRgSBH/toyota-640x426.webp" alt="" />
+                  <div className="absolute top-1/4 left-[70%]">
+                      <img className="w-1/2 lg:w-1/3 rounded-full" src="https://i.ibb.co/DD6xMJ8/discount.webp" alt="" />
+                  </div>
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+                  <img src="https://i.ibb.co/xh5FTPW/bmw3.jpg" alt="" />
+                  <div className="absolute top-1/4 left-[70%]">
+                      <img className="w-1/2 lg:w-1/3 rounded-full" src="https://i.ibb.co/DD6xMJ8/discount.webp" alt="" />
+                  </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
 
+export default Slider;
