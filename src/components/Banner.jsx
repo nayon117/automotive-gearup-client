58af51cdcc2a8088d205fc33d612a72589906ad8
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-21 08:14:13 +0600

    implement simple typewriter functionality in banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,22 @@
+import { useTypewriter } from 'react-simple-typewriter'
 const Banner = () => {
+  
+  const [typeEffect] = useTypewriter({
+    words: ['Dreams', 'with', 'Automotive', 'Excellence!'],
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed:50
+  })
+
   return (
     <div className=" min-h-[80vh] pt-12  ">
       <div className="flex flex-col-reverse md:flex-row items-center gap-8">
         <div className="flex-1 space-y-6">
-          <h2 className="text-3xl md:text-5xl font-semibold">Drive Your Dreams</h2>
+          <h2 className="text-3xl md:text-5xl font-semibold">
+            Drive Your  
+            <span className='font-bold text-red-500 ml-2'>{typeEffect}</span>
+             
+          </h2>
           <p className="text-lg">Explore a world of possibilities with our extensive range of quality vehicles and expert automotive solutions. Your dream car is just a click away.</p>
           <button className="btn btn-sm btn-neutral  ">Get Started</button>
         </div>
