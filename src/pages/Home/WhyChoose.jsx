8f0b8b25283c080186b5110af31d93851b5e6ce3
nayon117 <hasibul.nayon1@gmail.com>
2024-03-17 09:23:55 +0600

    update whychoose us page

--- src/pages/Home/WhyChoose.jsx
+++ src/pages/Home/WhyChoose.jsx
@@ -5,15 +5,12 @@ import { BiDollar } from "react-icons/bi";
 const WhyChoose = () => {
   return (
     <div className="my-12">
-      <section className="relative block px-6 py-10 md:py-20 md:px-10  border-t border-b border-neutral-900">
-        <div className="relative mx-auto max-w-5xl text-center">
-          <span className=" text-3xl font-semibold my-3 flex items-center justify-center  uppercase tracking-wider">
+      <section className="relative block  py-10 md:py-20 md:px-10  border-t border-b section-container">
+        <div className="relative section-container text-center">
+          <span className="title">
             Why choose us
           </span>
-          <h2 className="block w-full bg-gradient-to-b from-white to-gray-600 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">
-            Discover excellence and convenience with us
-          </h2>
-          <p className="mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-gray-500">
+          <p className="subtitle mt-3">
             We provide top-tier service and seamless experiences, making your
             automotive journey truly exceptional.
           </p>
@@ -22,8 +19,8 @@ const WhyChoose = () => {
         <div className="relative mx-auto max-w-7xl z-10 grid grid-cols-1 gap-10 pt-14 sm:grid-cols-2 lg:grid-cols-3">
           <div className="rounded-md border border-neutral-800   p-8 text-center shadow">
             <TbBrandBandlab className="text-3xl mx-auto"></TbBrandBandlab>
-            <h3 className="mt-6 text-gray-700">Wide range of brands</h3>
-            <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-500">
+            <h3 className="mt-6 text-gray-800">Wide range of brands</h3>
+            <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-600">
               Discover a diverse collection of top automotive brands, providing
               options to suit every taste and requirement
             </p>
@@ -31,8 +28,8 @@ const WhyChoose = () => {
 
           <div className="rounded-md border border-neutral-800  p-8 text-center shadow">
             <VscWorkspaceTrusted className="text-3xl mx-auto"></VscWorkspaceTrusted>
-            <h3 className="mt-6 text-gray-700">Trusted by our clients</h3>
-            <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-500">
+            <h3 className="mt-6 text-gray-800">Trusted by our clients</h3>
+            <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-600">
               Our commitment to excellence has earned us the trust and loyalty
               of our valued clients.
             </p>
@@ -40,8 +37,8 @@ const WhyChoose = () => {
 
           <div className="rounded-md border border-neutral-800  p-8 text-center shadow">
             <BiDollar className="text-3xl mx-auto"></BiDollar>
-            <h3 className="mt-6 text-gray-700">Fast & easy financing</h3>
-            <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-500">
+            <h3 className="mt-6 text-gray-800">Fast & easy financing</h3>
+            <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-600">
               Experience the convenience of our fast and easy car financing
               solutions. Get behind the wheel of your dream car in no time.
             </p>
