0ae3b9c3726b1a9dc4a4e4c3e59a9264fc70384f
nayon117 <hasibul.nayon1@gmail.com>
2024-04-05 06:01:35 +0600

    show orderlist in tabuler format

--- src/pages/Dashboard/User/OrderList.jsx
+++ src/pages/Dashboard/User/OrderList.jsx
@@ -1,21 +1,66 @@
 import { useEffect, useState } from "react";
 import useAuth from "../../../hooks/useAuth";
 
-
 const OrderList = () => {
-    const [orderList, setOrderList] = useState([]);
-    const { user } = useAuth();
-    useEffect(() => {
-        fetch(`http://localhost:5000/bookings?userEmail=${user?.email}`)
-          .then((res) => res.json())
-          .then((data) => setOrderList(data));
-      }, [user?.email]);
-      console.log(orderList);
-    
-    return(
-        <div>
-             <p> Working on this page </p>
+  const [orderList, setOrderList] = useState([]);
+  const { user } = useAuth();
+  useEffect(() => {
+    fetch(`http://localhost:5000/bookings?userEmail=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => setOrderList(data));
+  }, [user?.email]);
+  console.log(orderList);
+
+  return (
+    <div>
+      <div className="">
+        <div className="overflow-x-auto">
+          <table className="min-w-[90%] shadow-md border mx-auto border-gray-100 my-6">
+            <thead>
+              <tr className="bg-second text-white">
+                <th className="py-4 px-6 text-lg text-left border-b">
+                  Product Image
+                </th>
+                <th className="py-4 px-6 text-lg text-left border-b">
+                  Product Name
+                </th>
+                <th className="py-4 px-6 text-lg text-left border-b">Price</th>
+                <th className="py-4 px-6 text-lg border-b text-end">Action</th>
+                <th className="py-4 px-6 text-lg border-b text-end">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                orderList.map((order, index) => (
+                  <tr key={index} className="bg-gray-100">
+                    <td className="py-4 px-6 border-b">
+                      <img
+                        className="w-20 h-20 object-cover"
+                        src={order?.image}
+                        alt=""
+                      />
+                    </td>
+                    <td className="py-4 px-6 border-b">{order?.name}</td>
+                    <td className="py-4 px-6 border-b">{order?.price}</td>
+                    <td className="py-4 px-6 border-b text-end">
+                      <button className="bg-red-500 text-white px-4 py-1 rounded-md">
+                        Processing
+                      </button>
+                    </td>
+                    <td className="py-4 px-6 border-b text-end">
+                      <button className="bg-red-500 text-white px-4 py-1 rounded-md">
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              }
+            
+            </tbody>
+          </table>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default OrderList;
